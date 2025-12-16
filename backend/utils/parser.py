88145282c7cb14aacbdcc77e4bc0f856b17787b0
commis <hbcqxb@163.com>
2025-12-16 09:00:54 +0800

    change ip to domain

--- backend/utils/parser.py
+++ backend/utils/parser.py
@@ -10,7 +10,7 @@ logger = LoggerFactory.get_logger(__name__)
 
 
 class Parser:
-    _live_url = "http://107.174.95.154/tvbox/json/live.txt"
+    _live_url = "http://ak3721.top/tvbox/json/live.txt"
 
     @staticmethod
     def get_channel_data(text_data: str) -> list:
