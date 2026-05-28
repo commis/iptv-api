0f2206e14bb0d32bdc602af73a6dbf77ae31830e
bing.xu <bing.xu@lingshu.net>
2026-05-29 00:08:44 +0800

    修改配置

--- backend/api/site/routes.py
+++ backend/api/site/routes.py
@@ -1,15 +1,17 @@
+import re
 from typing import Optional
+from urllib.parse import quote, urlencode, unquote
 
 import httpx
-from fastapi import APIRouter, Query, BackgroundTasks, Path
-from httpx import ReadError, RemoteProtocolError
+import starlette
+from fastapi import APIRouter, Query, Request, BackgroundTasks, Path
 from starlette import status
 from starlette.responses import RedirectResponse, StreamingResponse
 
 from core.logger_factory import LoggerFactory
 from models.api_request import UpdateVodRequest
 from models.api_response import TaskResponse, ApiResponse
-from services import task_manager
+from services import task_manager, config_manager
 from services.spider.factory import SpiderFactory
 from utils.handler import handle_exception
 
@@ -74,19 +76,19 @@ async def api_collect(request: UpdateVodRequest, background_tasks: BackgroundTas
         handle_exception(f"update vod video request failed.")
 
 
-@router.get("/{sp}/{id}", summary="解析视频播放地址")
+@router.get("/player/{sp}/{id}", summary="解析视频播放地址")
 async def parse_channel_url(
         sp: str = Path(..., description="视频源，如 v-youtub"),
         id: str = Path(..., description="频道ID，例如：4fkoZ7z5ggM"),
         tp: Optional[str] = Query(None, description="返回的数据类型，例如：json")
 ):
-    logger.debug(f"parse: sp={sp}, id={id}")
-    resp_data = {"op": "parse", "sp": sp, "id": id, "type": tp}
+    logger.debug(f"player: sp={sp}, id={id}")
+    resp_data = {"op": "player", "sp": sp, "id": id, "type": tp}
     resp_message = "失败解析播放地址"
 
     spider = SpiderFactory.get_spider(sp)
     if not spider or not spider.config:
-        logger.error(f"vod: sp={sp}对应的站点配置不存在")
+        logger.error(f"player: sp={sp}对应的站点配置不存在")
         return ApiResponse(code=400, message=resp_message, data=resp_data)
 
     try:
@@ -115,18 +117,18 @@ async def parse_channel_url(
     return ApiResponse(code=101, message=resp_message, data=resp_data)
 
 
-@router.get("/proxy/{sp}/{id}", summary="代理视频播放地址")
-async def proxy_channel_url(
+@router.get("/m3u8/{sp}/{id}", summary="视频播放 seg_ts 地址")
+async def get_ts_url(
         sp: str = Path(..., description="视频源，如 v-youtub"),
         id: str = Path(..., description="频道ID，例如：4fkoZ7z5ggM")
 ):
-    logger.debug(f"proxy: sp={sp}, vid={id}")
-    resp_data = {"op": "proxy", "sp": sp, "id": id}
-    resp_message = "失败代理播放地址"
+    logger.debug(f"m3u8: sp={sp}, id={id}")
+    resp_data = {"op": "m3u8", "sp": sp, "id": id}
+    resp_message = "失败m3u8播放地址"
 
     spider = SpiderFactory.get_spider(sp)
     if not spider or not spider.config:
-        logger.error(f"vod: sp={sp}对应的站点配置不存在")
+        logger.error(f"m3u8: sp={sp}对应的站点配置不存在")
         return ApiResponse(code=400, message=resp_message, data=resp_data)
 
     try:
@@ -139,33 +141,70 @@ async def proxy_channel_url(
                 spider.redis_set(redis_key, real_player, ex=-1)
 
         if real_player:
-            json_data = spider.get_player_json(1, id, real_player["url"])
-            player_url = json_data.pop("url")
-            async with httpx.AsyncClient(timeout=20) as client:
-                resp = await client.send(
-                    client.build_request("GET", player_url, headers=json_data["header"]),
-                    stream=True)
-                resp_headers = {
-                    "Content-Type": resp.headers.get("content-type", "video/mp4"),
-                    "Accept-Ranges": "bytes",
-                }
-                cl = resp.headers.get("content-length")
-                if cl and cl.isdigit():
-                    resp_headers["Content-Length"] = cl
+            player_url = real_player.pop("url")
+            json_data = spider.get_player_json(1, id, player_url)
+            async with (httpx.AsyncClient(timeout=20) as client):
+                resp = await client.get(player_url, headers=json_data["header"])
+                resp.raise_for_status()
+                raw_m3u8 = resp.text
 
-                async def safe_stream():
-                    try:
-                        async for chunk in resp.aiter_bytes():
-                            yield chunk
-                    except (ReadError, RemoteProtocolError) as e:
-                        # logger.warning(f"Stream interrupted for {id}: {e}")
-                        pass
+                base_url = config_manager.service_params.url_parse.split('/m3u8')[0]
 
-                return StreamingResponse(
-                    safe_stream(),
-                    status_code=resp.status_code,
-                    headers=resp_headers,
+                def replace_ts_url(match):
+                    original_url = match.group(0)
+                    proxy_url = f"{base_url}/proxy/{sp}?url={quote(original_url, safe='')}"
+                    return proxy_url
+
+                modified_m3u8 = re.sub(r'https?://[^\s#]+?/seg\.ts[^\s#]*', replace_ts_url, raw_m3u8)
+                return starlette.responses.Response(
+                    content=modified_m3u8,
+                    media_type="application/vnd.apple.mpegurl"
                 )
+    except Exception as e:
+        logger.error(f"proxy {id} video failed.", exc_info=False)
+    return ApiResponse(code=101, message=resp_message, data=resp_data)
+
+
+@router.get("/proxy/{sp}", summary="代理视频流播放")
+async def proxy_ts_url(
+        request: Request,
+        sp: str = Path(..., description="视频源，如 v-youtub"),
+        url: str = Query(..., description="播放 ts 地址")
+):
+    logger.debug(f"proxy: sp={sp}, url={url}")
+    resp_data = {"op": "set_ts", "sp": sp}
+    resp_message = "失败代理视频流播放"
+
+    spider = SpiderFactory.get_spider(sp)
+    if not spider or not spider.config:
+        logger.error(f"proxy: sp={sp}对应的站点配置不存在")
+        return ApiResponse(code=400, message=resp_message, data=resp_data)
+
+    player_url = unquote(url)
+    headers = spider.get_player_json(1, -1, "").pop("header")
+    range_header = request.headers.get("range")
+    if range_header:
+        headers["Range"] = range_header
+
+    try:
+        async with (httpx.AsyncClient(timeout=20) as client):
+            resp = await client.get(player_url, headers=headers)
+            resp.raise_for_status()
+
+            resp_headers = {
+                "Content-Type": "video/mp2t",
+                "Accept-Ranges": "bytes",
+            }
+            if "content-length" in resp.headers:
+                resp_headers["Content-Length"] = resp.headers["content-length"]
+            if "content-range" in resp.headers:
+                resp_headers["Content-Range"] = resp.headers["content-range"]
+
+            return StreamingResponse(
+                resp.aiter_bytes(),
+                status_code=resp.status_code,
+                headers=resp_headers,
+            )
     except Exception as e:
         logger.error(f"proxy {id} video failed.", exc_info=False)
     return ApiResponse(code=101, message=resp_message, data=resp_data)
